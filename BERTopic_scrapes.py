94fae4cdc32e2572ab92b35fa41173ebd69edb67
dcs0spb <s.p.bradley@durham.ac.uk>
2024-05-02 18:40:06 +0100

    Stop words for BERTopic

--- BERTopic_scrapes.py
+++ BERTopic_scrapes.py
@@ -1,11 +1,17 @@
 from bertopic import BERTopic
+from sklearn.feature_extraction.text import CountVectorizer
+
+
 import json
 import pandas as pd
 
 with open("scraping/topic_text.json", "r") as file:
     topic_texts = json.load(file)
 
-topic_model = BERTopic()
+
+vectorizer_model = CountVectorizer(stop_words="english")
+
+topic_model = BERTopic(vectorizer_model=vectorizer_model)
 topics, probs = topic_model.fit_transform(topic_texts)
 topic_model.get_topic_info().to_json("topic_model_info.json")
 
