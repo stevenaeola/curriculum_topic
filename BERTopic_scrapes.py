9618c93a69e09437f3ce1b8b628ff930c2ab3e02
dcs0spb <s.p.bradley@durham.ac.uk>
2024-05-02 10:27:04 +0100

    Save topic modelling results

--- BERTopic_scrapes.py
+++ BERTopic_scrapes.py
@@ -8,3 +8,6 @@ with open("scraping/topic_text.json", "r") as file:
 topic_model = BERTopic()
 topics, probs = topic_model.fit_transform(topic_texts)
 topic_model.get_topic_info().to_json("topic_model_info.json")
+
+with open("scraping/topic_assignment.json", "w") as file:
+    json.dump(topics, file)
