d7859a3e0b0ce9cf7d58fcf6e4151a27aed96c29
dcs0spb <s.p.bradley@durham.ac.uk>
2024-05-02 10:20:27 +0100

    Removing unsuccessful scraping results

--- BERTopic_scrapes.py
+++ BERTopic_scrapes.py
@@ -1,9 +1,10 @@
 from bertopic import BERTopic
 import json
+import pandas as pd
 
 with open("scraping/topic_text.json", "r") as file:
     topic_texts = json.load(file)
 
 topic_model = BERTopic()
 topics, probs = topic_model.fit_transform(topic_texts)
-print(topic_model.get_topic_info())
+topic_model.get_topic_info().to_json("topic_model_info.json")
