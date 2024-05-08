f99a80baa37ed65c45fe75a5fc470303728610d4
stevenaeola <s.p.bradley@durham.ac.uk>
2024-05-08 14:25:34 +0100

    Include WG6 modules in topic model

--- BERTopic_scrapes.py
+++ BERTopic_scrapes.py
@@ -8,10 +8,12 @@ import pandas as pd
 with open("scraping/topic_text.json", "r") as file:
     topic_texts = json.load(file)
 
+with open("iticse2023_wg6_electives_non_uk.json", "r") as file:
+    iticse_texts = json.load(file)
 
-vectorizer_model = CountVectorizer(stop_words="english")
+vectorizer_model = CountVectorizer(stop_words="english", ngram_range = (1, 2))
 
-topic_model = BERTopic(vectorizer_model=vectorizer_model)
+topic_model = BERTopic(vectorizer_model=vectorizer_model, min_topic_size=5)
 topics, probs = topic_model.fit_transform(topic_texts)
 topic_model.get_topic_info().to_json("scraping/topic_model_info.json")
 
