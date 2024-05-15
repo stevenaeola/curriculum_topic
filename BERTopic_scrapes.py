cfca8215c6d0ab0dd7fa4706276b491c560d0bc7
stevenaeola <s.p.bradley@durham.ac.uk>
2024-05-15 23:28:27 +0100

    Run topic model with stop words

--- BERTopic_scrapes.py
+++ BERTopic_scrapes.py
@@ -40,13 +40,18 @@ vectorizer_model = CountVectorizer(stop_words="english", ngram_range = (1, 2))
 topic_model = BERTopic(vectorizer_model=vectorizer_model, min_topic_size=5)
 topics, probs = topic_model.fit_transform(topic_texts + iticse_texts)
 topic_model.get_topic_info().to_json("topic_model_info.json")
-topic_model.visualize_topics()
+fig = topic_model.visualize_topics()
+fig.write_html("topic_viz.html")
 
 with open("scraping/topic_assignment.json", "w") as file:
-    json.dump(topics[:len(topic_texts)], file)
+    json.dump(topics[:len(topic_texts)], file, indent=1)
 
 with open("iticse_topic_assignment.json", "w") as file:
-    json.dump(topics[len(topic_texts):], file)
+    json.dump(topics[len(topic_texts):], file, indent=1)
 
-hierarchical_topics = topic_model.hierarchical_topics(docs)
-topic_model.visualize_hierarchy(hierarchical_topics=hierarchical_topics)
+topic_model.save("serialized", serialization="safetensors", save_ctfidf=True, save_embedding_model=embedding_model)
+
+hierarchical_topics = topic_model.hierarchical_topics(topic_texts + iticse_texts)
+fig = topic_model.visualize_hierarchy(hierarchical_topics=hierarchical_topics)
+fig.write_html("hierarchy_viz.html")
+
